5484042574c0599eeb19e05055716fafbfc9dbb5
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-15 14:40:03 +0200

    3.3th preparing container

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,5 +14,16 @@ bg-dark
 
 @section('content')
 
+<div id="jumbotron" ></div>
+
+<div id="cards-container" class="container" >
+<span id="absolute" >current series</span>
+<h1 style='color: white;'>content goes here</h1>
+<div id="cards-row" class="row">
+    
+</div>
+</div>
+
+
 
 @endsection
