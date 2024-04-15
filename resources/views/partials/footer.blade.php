bd32493bfdce8a2f7fad54df06b79d48f36ad496
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-15 13:53:55 +0200

    2nd styling shared app

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,6 @@
-  <footer class="d-flex">
+  <footer>
+
+<div id="sup-footer" >
 
         <div class="container d-flex justify-content-between ">
             <div class="wrapper">
@@ -23,5 +25,60 @@
                 <a>digital comics</a>
             </div>
         </div>
+</div>
+
+         <div id="background"></div>
+
+    <div id="sub-footer">
+        <div class="container">
+
+            <div id="wrapper" class="d-flex flex-row justify-content-start align-items-start ">
+                <div class="d-flex flex-column justify-content-between align-items-start">
+                    <ul class="list-unstyled">
+                        <h5>dc comics</h5>
+                        <li href="">characters</li>
+                        <li href="">comics</li>
+                        <li href="">movies</li>
+                        <li href="">TV</li>
+                        <li href="">games</li>
+                        <li href="">videos</li>
+                        <li href="">news</li>
+                    </ul>
+                    <ul class="list-unstyled">
+                        <h5>shop</h5>
+                        <li href="">shop DC</li>
+                        <li href="">shop DC collectibles</li>
+                    </ul>
+                </div>
+                <div class="d-flex flex-column justify-content-between align-items-start ">
+                    <ul class="list-unstyled">
+                        <h5>dc</h5>
+                        <li href="">terms of use</li>
+                        <li href="">privacy policy (new)</li>
+                        <li href="">add choices</li>
+                        <li href="">advertising</li>
+                        <li href="">jobs</li>
+                        <li href="">subscriptions</li>
+                        <li href="">talent workshops</li>
+                        <li href="">CPSC certificates</li>
+                        <li href="">ratings</li>
+                        <li href="">shop help</li>
+                        <li href="">contact us</li>
+                    </ul>
+                </div>
+                <div class="d-flex flex-column justify-content-between align-items-start">
+                    <ul class="list-unstyled">
+                        <h5>sites</h5>
+                        <li href="">DC</li>
+                        <li href="">MAD magazine</li>
+                        <li href="">DC kids</li>
+                        <li href="">DC universe</li>
+                        <li href="">DC power visa</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
     </footer>
