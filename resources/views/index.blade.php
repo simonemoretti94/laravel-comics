8603241ec75d21c5aa21237caa3cc6519e7a146b
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-16 12:13:21 +0200

    3.6th ready for styling cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,9 +21,18 @@ bg-dark
     {{-- <h1>content goes here</h1> --}}
     <div id="cards-row" class="row">
         @foreach ($comics as $comic)
-            <div id="col" class="col-lg-3 col-md-6 col-sm-12">
-                <div id="col-wrapper">
+            <div id="col" class="col-lg-2 col-md-4 col-sm-6">
+                <div id="col-wrapper" class="card">
+                    <div class="card-body">
+                        <img
+                            src="{{$comic['thumb']}}"
+                            class="img-fluid rounded-top"
+                            alt="{{$comic['title']}}"
+                        />  
+                    </div>
+                    <div class="card-footer">
                         <p>{{$comic['title']}}</p>
+                    </div>
                 </div>
             </div>
         @endforeach
