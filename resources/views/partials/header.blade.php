f3f6a89f465ab26884dbef2723f917e15133751b
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-21 23:03:38 +0200

    3.6th styling header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header>
-    <div class="container d-flex justify-content-between align-items-center">
-        <a href="{{route('index')}}" ><img id="logo" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt=""/></a>
+    <div class="container d-flex align-items-center {{!Route::currentRouteName('index') ? 'justify-content-center' : 'justify-content-between'}}">
+        <a href="{{route('index')}}"><img id="logo" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo" class="{{!Route::currentRouteName('index') ? 'd-none' : ''}}"/></a>
         <ul>
+            <a href="{{route('index')}}" class="{{!Route::currentRouteName('index') ? '' : 'd-none'}} text-dark" style="font-size: 20px"><strong><i>Home</i></strong></a>
             <a href="{{route('json-api')}}" class="{{!Route::currentRouteName('index') ? 'd-none' : ''}}">json</a>
             <a href="#" class="{{!Route::currentRouteName('index') ? 'd-none' : ''}}">characters</a>
             <a href="#" class="{{!Route::currentRouteName('index') ? 'd-none' : ''}}">comics</a>
